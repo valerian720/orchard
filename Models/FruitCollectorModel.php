2d94ffe187a6c606e8c9b3311c9307b50b3aa625
Валериан Воротников <valerian7202@gmail.com>
2023-01-16 19:01:29 +0300

    init basic struct

---
+++ Models/FruitCollectorModel.php
@@ -0,0 +1,25 @@
+//DB struct
+
+/*
+
+
+*/
+
+// objects
+/*
+- garden
+- fruit collector
+- apple tree
+- apple
+- pear tree
+- pear
+
+*/
+
+
+// ***************
+
+// add trees
+// grow fruit on trees
+// collect products
+//
---
+++ main.php
@@ -0,0 +1,30 @@
+//input struct
+
+/*
+- apple tree count
+- pear tree count
+- minmax apple count per tree
+- minmax pear count per tree
+- minmax apple weight
+- minmax pear weight
+*/
+
+// objects
+/*
+- garden
+- fruit collector
+- apple tree
+- apple
+- pear tree
+- pear
+*/
+
+
+// ***************
+
+// add trees
+// grow fruit on trees
+// collect products
+// count products per type
+// weight products
+//
